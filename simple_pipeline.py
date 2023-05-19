47386ede78c0981a8d9e2ff0042dcf5b43ec040a
Davide Belfiori <davidebelfiori@gmail.com>
2023-05-19 11:06:37 +0200

    Add model serving component

--- simple_pipeline.py
+++ simple_pipeline.py
@@ -1,6 +1,9 @@
 """
-    Simple Kubeflow Pipeline.
+    End-to-End Kubeflow Pipeline, from data loading to model serving.
 """
+
+# TODOs: 
+#   - add minio_access_key and minio_secret_key as parameter for all components
 
 # --------------
 # --- IMPORT ---
@@ -21,7 +24,7 @@ def load_dataset(m: float = 0.5,
                  test_size: float = 0.3,
                  random_state: int = 123,
                  sep: str = ',',
-                 minio_endpoint: str = "minio-service.kubeflow.svc.cluster.local",
+                 minio_endpoint: str = "minio-service.kubeflow",
                  bucket_name: str = "datasets",
                  update_dataset: bool = False):
     """
@@ -84,7 +87,7 @@ def load_dataset(m: float = 0.5,
         print("--- Data csv saved to MinIO location simple_pipeline_dataset/ ---")
 
 def print_data_desc(sep: str = ',', 
-                    minio_endpoint: str = "minio-service.kubeflow.svc.cluster.local", 
+                    minio_endpoint: str = "minio-service.kubeflow", 
                     bucket_name: str = "datasets") -> NamedTuple('PrintDataOutput', [('mlpipeline_ui_metadata', 'UI_metadata')]):
     """
     Print Dataset description.
@@ -156,7 +159,7 @@ def print_data_desc(sep: str = ',',
 def train_model(sep: str = ',',
                 save_model: bool = True,
                 model_name: str = "simple_pipeline_model",
-                minio_endpoint: str = "minio-service.kubeflow.svc.cluster.local",
+                minio_endpoint: str = "minio-service.kubeflow",
                 data_bucket: str = "datasets",
                 model_bucket: str = "models") -> NamedTuple('TrainModelOutput', [('mlpipeline_ui_metadata', 'UI_metadata')]):
     """
@@ -199,7 +202,7 @@ def train_model(sep: str = ',',
     # >>> SAVE MODEL
     if save_model:
         # Dump model
-        model_filename = "data/" + model_name + ".sav"
+        model_filename = "data/" + model_name + ".joblib"
         joblib.dump(model, filename=model_filename)
         # Upload to MinIO
         if not minio_client.bucket_exists(bucket_name=model_bucket):
@@ -207,11 +210,11 @@ def train_model(sep: str = ',',
             minio_client.make_bucket(bucket_name=model_bucket)
         try:
             minio_client.fput_object(bucket_name = model_bucket, 
-                                    object_name = "simple_pipeline/"+ model_name + ".sav",
+                                    object_name = "simple_pipeline/"+ model_name + ".joblib",
                                     file_path = model_filename)
         except:
             raise RuntimeError("Error while uploading model.") 
-        print("--- Model saved to MinIO location simple_pipeline_dataset/ ---")
+        print("--- Model saved to MinIO location simple_pipeline/ ---")
 
     # >>> VISUALIZE MODEL
     score = model.score(X_train, Y_train)
@@ -236,7 +239,7 @@ def train_model(sep: str = ',',
     output = namedtuple('TrainModelOutput', ['mlpipeline_ui_metadata'])
     return output(json.dumps(metadata))
 
-def eval_model(minio_endpoint: str = "minio-service.kubeflow.svc.cluster.local",
+def eval_model(minio_endpoint: str = "minio-service.kubeflow",
                data_bucket: str = "datasets",
                sep: str = ',',
                model_bucket: str = "models",
@@ -268,8 +271,8 @@ def eval_model(minio_endpoint: str = "minio-service.kubeflow.svc.cluster.local",
                                  object_name="simple_pipeline_dataset/test_data.csv",
                                  file_path="data/test_data.csv")
         minio_client.fget_object(bucket_name=model_bucket,
-                                 object_name="simple_pipeline/"+ model_name + ".sav",
-                                 file_path="data/" + model_name + ".sav")
+                                 object_name="simple_pipeline/"+ model_name + ".joblib",
+                                 file_path="data/" + model_name + ".joblib")
     except:
         raise RuntimeError("Error while loading dataset and model.")
 
@@ -279,7 +282,7 @@ def eval_model(minio_endpoint: str = "minio-service.kubeflow.svc.cluster.local",
     Y_test = test_df.loc[:,"Y"].values.reshape(-1, 1)
 
     # >>> LOAD MODEL
-    model = joblib.load(filename="data/" + model_name + ".sav")
+    model = joblib.load(filename="data/" + model_name + ".joblib")
 
     # >>> EVAL MODEL
     score = model.score(X=X_test, y=Y_test)
@@ -313,6 +316,92 @@ def eval_model(minio_endpoint: str = "minio-service.kubeflow.svc.cluster.local",
     
     output = namedtuple('EvalModelOutput', ['mlpipeline_ui_metadat', 'mlpipeline_metrics'])
     return output(json.dumps(metadata), json.dumps(metrics))
+
+def serve_model(service_name: str = "simple-serving",
+                add_service_version: bool = True,
+                namespace: str = None,
+                kserve_version: str = "v1beta1",
+                model_bucket: str = "models",
+                model_name: str = "simple_pipeline_model",
+                service_account_name: str = "kserve-service-credentials",
+                update_credentials: bool = False,
+                minio_endpoint: str = "minio-service.kubeflow",
+                minio_access_key: str = "minio",
+                minio_secret_key: str = "minio123",
+                access_key_name: str = None,
+                secret_key_name: str = None,
+                profile: str = "default",
+                use_https: bool = False,
+                verify_ssl: bool = False):
+    """
+    Create an InferenceService for the trained model.
+    """
+    # >>> IMPORT
+    from kserve import (
+        KServeClient, 
+        utils, 
+        constants, 
+        V1beta1SKLearnSpec, 
+        V1beta1PredictorSpec,
+        V1beta1InferenceServiceSpec,
+        V1beta1InferenceService
+    )
+    from kubernetes import client
+    import base64
+    import datetime
+
+    KServe = KServeClient()
+    if namespace == None:
+        namespace = utils.get_default_target_namespace()
+
+    # >>> MINIO CREDENTIALS
+    if update_credentials:
+        # Encode access and secret key in base64
+        b64_access_key = base64.b64encode(minio_access_key.encode("ascii"))
+        b64_access_key = b64_access_key.decode("ascii")
+        b64_secret = base64.b64encode(minio_secret_key.encode("ascii"))
+        b64_secret = b64_secret.decode("ascii")
+        # Write credentials
+        if access_key_name == None or access_key_name == "":
+            access_key_name = constants.S3_ACCESS_KEY_ID_DEFAULT_NAME
+        if secret_key_name == None or secret_key_name == "":
+            secret_key_name = constants.S3_SECRET_ACCESS_KEY_DEFAULT_NAME
+        credentials = "[{profile}]\n{access_key_name}={access_key}\n{secret_key_name}={secret_key}".format(
+            profile = profile,
+            access_key_name = access_key_name,
+            access_key = b64_access_key, 
+            secret_key_name = secret_key_name,
+            secret_key = b64_secret)
+        with open("data/credentials", "wt") as cred_file:
+            cred_file.write(credentials)
+        KServe.set_credentials(storage_type='S3',
+                                namespace=namespace,
+                                credentials_file='data/credentials',
+                                service_account=service_account_name,
+                                s3_profile=profile,
+                                s3_endpoint=minio_endpoint+":9000",
+                                s3_use_https='1' if use_https else '0',
+                                s3_verify_ssl='1' if verify_ssl else '0')
+        
+    # >>> INFERENCE SERVICE OPTIONS
+    api_version = constants.KSERVE_GROUP + '/' + kserve_version
+    storage_uri = "s3://{}/simple_pipeline/{}.joblib".format(model_bucket, model_name)
+    if add_service_version:
+        service_name = service_name + "-" + datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+
+    # >>> INFERENCE SERVICE DEFINITION
+    metadata = client.V1ObjectMeta(name = service_name, namespace = namespace)
+    sklearn_spec = V1beta1SKLearnSpec(storage_uri = storage_uri)
+    predictor = V1beta1PredictorSpec(sklearn = sklearn_spec, service_account_name = service_account_name)
+    spec = V1beta1InferenceServiceSpec(predictor = predictor)
+    isvc = V1beta1InferenceService(api_version = api_version,
+                                   kind = constants.KSERVE_KIND,
+                                   metadata = metadata,
+                                   spec = spec)
+    
+    # >>> INFERENCE SERVICE CREATION
+    info = KServe.create(isvc)
+    print(info)
 
 # ----------------
 # --- PIPELINE ---
@@ -352,15 +441,19 @@ eval_model_component = components.create_component_from_func(
                            "scikit-learn==1.2.2",
                            "joblib==1.2.0"]
 )
+serve_model_component = components.create_component_from_func(
+    func = serve_model,
+    base_image = "python:3.8",
+    packages_to_install = ["kserve==0.10.1",
+                           "kubernetes==25.3.0"]
+)
 
 # > PIPELINE
 @dsl.pipeline(
         name="simple-pipeline",
         description="Simple pipeline example."
 )
-def simple_pipeline(attach_volume: bool = True,
-                    data_path: str = "/data",
-                    m: float = 0.5,
+def simple_pipeline(m: float = 0.5,
                     q: float = 1.0,
                     noise_mean: float = 0,
                     noise_scale: float = 0.1,
@@ -370,75 +463,75 @@ def simple_pipeline(attach_volume: bool = True,
                     sep: str = ',',
                     save_model: bool = True,
                     model_name: str = "simple_pipeline_model",
-                    minio_endpoint: str = "minio-service.kubeflow.svc.cluster.local",
+                    minio_endpoint: str = "minio-service.kubeflow",
                     data_bucket: str = "datasets",
-                    model_bucket: str = "models"):
+                    model_bucket: str = "models",
+                    inference_service_name: str = "simple-serving",
+                    add_inference_service_version: bool = True,
+                    namespace: str = None,
+                    kserve_version: str = "v1beta1",
+                    service_account_name: str = "kserve-service-credentials",
+                    update_credentials: bool = False,
+                    access_key_name: str = None,
+                    secret_key_name: str = None,
+                    profile: str = "default",
+                    use_https: bool = False,
+                    verify_ssl: bool = False):
     """
     Pipeline function
     """
     # >>> Create a volume for this pipeline
-    if attach_volume:
-        vop = dsl.VolumeOp(name = "simple_pipeline_volume", 
-                            resource_name = "simple_pipeline_volume",
-                            size = "1Gi",
-                            modes = dsl.VOLUME_MODE_RWO)
-        # >>> Run tasks
-        load_data_task = load_dataset_component(m = m, 
-                                                q = q,
-                                                noise_mean = noise_mean,
-                                                noise_scale = noise_scale,
-                                                test_size = test_size,
-                                                random_state = random_state,
-                                                sep = sep,
+    vop = dsl.VolumeOp(name = "simple_pipeline_volume", 
+                        resource_name = "simple_pipeline_volume",
+                        size = "1Gi",
+                        modes = dsl.VOLUME_MODE_RWO)
+    # >>> Run tasks
+    load_data_task = load_dataset_component(m = m, 
+                                            q = q,
+                                            noise_mean = noise_mean,
+                                            noise_scale = noise_scale,
+                                            test_size = test_size,
+                                            random_state = random_state,
+                                            sep = sep,
+                                            minio_endpoint = minio_endpoint,
+                                            bucket_name = data_bucket,
+                                            update_dataset = update_dataset).add_pvolumes({"/data": vop.volume})
+    print_desc_task = print_data_desc_component(sep = sep,
                                                 minio_endpoint = minio_endpoint,
-                                                bucket_name = data_bucket,
-                                                update_dataset = update_dataset).add_pvolumes({data_path: vop.volume})
-        print_desc_task = print_data_desc_component(sep = sep,
-                                                    minio_endpoint = minio_endpoint,
-                                                    bucket_name = data_bucket).add_pvolumes({data_path: vop.volume}).after(load_data_task)
-        train_model_task = train_model_component(sep = sep,
-                                                save_model = save_model,
-                                                model_name = model_name,
-                                                minio_endpoint = minio_endpoint,
-                                                data_bucket = data_bucket,
-                                                model_bucket = model_bucket).add_pvolumes({data_path: vop.volume}).after(load_data_task)
-        eval_model_task = eval_model_component(sep = sep,
-                                                model_name = model_name,
-                                                minio_endpoint = minio_endpoint,
-                                                data_bucket = data_bucket,
-                                                model_bucket = model_bucket).add_pvolumes({data_path: vop.volume}).after(train_model_task)
-    else:
-        # >>> Run tasks
-        load_data_task = load_dataset_component(m = m, 
-                                                q = q,
-                                                noise_mean = noise_mean,
-                                                noise_scale = noise_scale,
-                                                test_size = test_size,
-                                                random_state = random_state,
-                                                sep = sep,
-                                                minio_endpoint = minio_endpoint,
-                                                bucket_name = data_bucket,
-                                                update_dataset = update_dataset)
-        print_desc_task = print_data_desc_component(sep = sep,
-                                                    minio_endpoint = minio_endpoint,
-                                                    bucket_name = data_bucket).after(load_data_task)
-        train_model_task = train_model_component(sep = sep,
-                                                save_model = save_model,
-                                                model_name = model_name,
-                                                minio_endpoint = minio_endpoint,
-                                                data_bucket = data_bucket,
-                                                model_bucket = model_bucket).after(load_data_task)
-        eval_model_task = eval_model_component(sep = sep,
-                                                model_name = model_name,
-                                                minio_endpoint = minio_endpoint,
-                                                data_bucket = data_bucket,
-                                                model_bucket = model_bucket).after(train_model_task)
-    
+                                                bucket_name = data_bucket).add_pvolumes({"/data": vop.volume}).after(load_data_task)
+    train_model_task = train_model_component(sep = sep,
+                                            save_model = save_model,
+                                            model_name = model_name,
+                                            minio_endpoint = minio_endpoint,
+                                            data_bucket = data_bucket,
+                                            model_bucket = model_bucket).add_pvolumes({"/data": vop.volume}).after(load_data_task)
+    eval_model_task = eval_model_component(sep = sep,
+                                            model_name = model_name,
+                                            minio_endpoint = minio_endpoint,
+                                            data_bucket = data_bucket,
+                                            model_bucket = model_bucket).add_pvolumes({"/data": vop.volume}).after(train_model_task)
+    serve_model_task = serve_model_component(service_name = inference_service_name,
+                                             add_service_version = add_inference_service_version,
+                                             namespace = namespace,
+                                             kserve_version = kserve_version,
+                                             model_bucket = model_bucket,
+                                             model_name = model_name,
+                                             service_account_name = service_account_name,
+                                             update_credentials = update_credentials,
+                                             minio_endpoint = minio_endpoint,
+                                             minio_access_key = "minio",
+                                             minio_secret_key = "minio123",
+                                             access_key_name = access_key_name,
+                                             secret_key_name = secret_key_name,
+                                             profile = profile,
+                                             use_https = use_https,
+                                             verify_ssl = verify_ssl).add_pvolumes({"/data": vop.volume}).after(eval_model_task)
     # >>> Disable caching
     load_data_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
     print_desc_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
     train_model_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
     eval_model_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
+    serve_model_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
     
 
 if __name__ == "__main__":
@@ -454,6 +547,7 @@ if __name__ == "__main__":
         print_data_desc(minio_endpoint = minio_endpoint)
         train_model(minio_endpoint = minio_endpoint)
         eval_model(minio_endpoint = minio_endpoint)
+        # serve_model(minio_endpoint = minio_endpoint)
     elif mode == "compile":
         # >>> Compile pipeline
         compiler.Compiler().compile(
